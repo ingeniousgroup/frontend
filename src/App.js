749a8693df81e1a013b02dbba7748adc231be78e
vijay <vishwakarma.vijay.1001@gmail.com>
2023-04-25 15:38:54 +0530

    property form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Components/Home/Home';
+import { Route, Routes } from 'react-router';
+import Signin from './Components/User/signin';
 
 function App() {
   return<>
     <Home/>
+    <Routes>
+      <Route path='/' element={<Signin/>}/>
+    </Routes>
    </>
 }
 
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,17 +4,25 @@ import Navbar from "../Headers.js/Navbar/navbar";
 import { useSelector } from "react-redux";
 import RecommendedProperties from "../House/RecommendedProperties";
 import Categories from "../House/Categories/categories";
+import Signin from "../User/signin";
+import PostProperty from "../House/PostProperty/postProperty";
+import Signup from "../User/signup";
 
 function Home(){
   return <>
    <div className="container-fluid">
-    
     <Navbar/>
     <div className="mtt"></div>
     <RecommendedProperties/>
     <div className="mtt"></div>
     <Categories/>
   </div>
+  <div className="mt-5"></div>
+  <PostProperty/>
+  <div className="mt-5"></div>
+  <Signin/>
+  <div className="mt-5"></div>
+  <Signup/>
   </>
 }
 
