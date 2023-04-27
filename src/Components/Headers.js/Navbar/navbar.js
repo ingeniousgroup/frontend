5286998e2d45df36a65094130faa7acf9037b113
vijay <vishwakarma.vijay.1001@gmail.com>
2023-04-27 19:49:02 +0530

    signin and signout added

--- src/Components/Headers.js/Navbar/navbar.js
+++ src/Components/Headers.js/Navbar/navbar.js
@@ -1,59 +1,85 @@
 import { useNavigate } from "react-router";
 import SearchBar from "./SerchBar";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { removeUser } from "../../../redux-config/UserSlice";
 
 
-function Navbar (){
+function Navbar() {
   const navigate = useNavigate();
-  const propertyPost = ()=>{
+  const propertyPost = () => {
     navigate("/propertypost");
   }
-
-  return<header className="main-header">
-         <div className="container-fluid">
-          <div className="navbardivBackgroundColor">
-          <nav className="navbar navbar-expand-lg main-nav px-0 ">
-            <div className="navbar-brand" >
-              <img src="images/logo.png" className="logoImg"/>
-              {/* <span> Kirayewala</span> */}
-            </div>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainMenu" aria-controls="mainMenu" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="icon-bar icon-bar-1" />
-              <span className="icon-bar icon-bar-2" />
-              <span className="icon-bar icon-bar-3" />
-            </button>
-            <div className="collapse navbar-collapse" id="mainMenu">
-              <ul className="navbar-nav ml-auto  f1">
-                <li>
-                  <a href="#home" className="active active-first">Home</a>
-                </li>
-                <li>
-                  <a href="#about">About </a>
-                </li>
-                <li>
-                  <a href="#service">Services</a>
-                </li>
-                <li>
-                  <button className="postPropertyButton" onClick={propertyPost}>
-                    Post property
-            
-                  </button>
-                </li>
-                <li>
-                  <a href="#contact">Contact</a>
-                </li>
-                <li className="userIconStyle">
-                <i className="fa fa-user-circle-o" aria-hidden="true" ></i>
-                </li>
-              </ul>
-            </div>
-          </nav>
-          <div className="navbarHelpdiv">
-           <SearchBar/>
-         </div>
+  const {currentUser} = useSelector((state)=>state.user);
+  const dispatch = useDispatch();
+  const signout =()=>{
+    dispatch(removeUser());
+  }
+  return <header className="main-header">
+    <div className="container-fluid">
+      <div className="navbardivBackgroundColor">
+        <nav className="navbar navbar-expand-lg main-nav px-0 ">
+          <div className="navbar-brand" >
+            <img src="images/logo.png" className="logoImg" />
+            {/* <span> Kirayewala</span> */}
           </div>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainMenu" aria-controls="mainMenu" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="icon-bar icon-bar-1" />
+            <span className="icon-bar icon-bar-2" />
+            <span className="icon-bar icon-bar-3" />
+          </button>
+          <div className="collapse navbar-collapse" id="mainMenu">
+            <ul className="navbar-nav ml-auto  f1">
+              <li>
+                <Link to='/' className=" active-first">Home</Link>
+              </li>
+              <li>
+                <a href="#about">About </a>
+              </li>
+              <li>
+                <a href="#service">Services</a>
+              </li>
+              <li>
+                <button className="postPropertyButton" onClick={propertyPost}>
+                  Post property
+
+                </button>
+              </li>
+              <li>
+                <a href="#contact">Contact</a>
+              </li>
+              <li className="userIconStyle">
+                
+                <div class="btn-group dropleft" style={{marginTop:"-1vh"}}>
+                  <button type="button" class="btn  dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i className="fa fa-user-circle-o icon" aria-hidden="true" ></i>    
+                  </button>
+                  <div class="dropdown-menu drop1" style={{minWidth:"9vh"}}>
+                    <ul>
+                      {!currentUser && <Link to='/signin'>
+                      <li style={{marginLeft:"-2vh",marginTop:"1vh"}}>
+                        signin
+                      </li>
+                      </Link>}
+                      {currentUser && <div onClick={signout}>
+                      <li style={{marginLeft:"-3vh",marginTop:"1vh"}}>
+                        signout
+                      </li>
+                      </div>}
+                    </ul>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <div className="navbarHelpdiv">
+          <SearchBar />
         </div>
-        {/* /.container */}
-      </header>
+      </div>
+    </div>
+    {/* /.container */}
+  </header>
 }
 
 export default Navbar;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,14 +4,17 @@ import Navbar from "../Headers.js/Navbar/navbar";
 import { useDispatch, useSelector } from "react-redux";
 import RecommendedProperties from "../House/RecommendedProperties";
 import Categories from "../House/Categories/categories";
-
+import PropertyLocation from "../House/PostProperty/PropertyLocation/propertyLocation";
 import PostProperty from "../House/PostProperty/post/postProperty";
 import Flate from "../House/PostPropertyForms/Flate/Flate";
 import { Route, Routes } from "react-router";
 import { useEffect } from "react";
 import { fetchPropertyList } from "../../redux-config/PropertySlice";
 import Property from "../House/Property/property";
-
+import ImagePost from "../House/PostProperty/PostImage/ImagePost";
+import Signin from "../User/signin";
+import Signup from "../User/signup";
+import ProtectedRoute from "../ProtectedRoute/ProtectedRoute";
 
 function Home(){
   const dispatch = useDispatch();
@@ -22,7 +25,7 @@ function Home(){
     <Navbar/>
     <div style={{marginTop:"100px"}}>
    {/* <RecommendedProperties/> */}
-   <Property/>
+   
     </div>
     
     <Routes>
@@ -34,7 +37,8 @@ function Home(){
         <Route path="office" element={<Flate/>}/>  
         <Route path="other" element={<Flate/>}/>
       </Route> */}
-        <Route path="/propertypost" element={<PostProperty/>}/>
+        <Route path="/" element={<Property/>}/>
+        <Route path="/propertypost" element={<ProtectedRoute><PostProperty/></ProtectedRoute>}/>
         <Route path="/villa" element={<Flate/>}/>
         <Route path="/plot" element={<Flate/>}/>
         <Route path="/formHouse" element={<Flate/>}/>
@@ -43,6 +47,8 @@ function Home(){
         <Route path="/flat" element={<Flate/>}/>
         <Route path="/CurrentLocation" element={<PropertyLocation/>}/>
         <Route path="/uploadImage" element={<ImagePost/>}/>
+        <Route path="/signin" element={<Signin/>}/>
+        <Route path="/signup" element={<Signup/>}/>
         
     </Routes>
     {/* <ImagePost/> */}
